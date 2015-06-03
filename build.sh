56c6a944f9fa6fc2f58375cb0c4d66600f2ff098
Emerson Lima <emersoncflima@gmail.com>
2015-06-03 09:51:36 -0300

    Initial Git Pages Setup

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn clean install
-unzip lotus-zip/target/lotus-tool.zip -d lotus-zip/target
--- lotus-plugin-simulator/src/main/java/br/uece/lotus/simulator/SimulatorWindowController.java
+++
@@ -1,7 +0,0 @@
-package br.uece.lotus.simulator;
-
-/**
- * Created by emerson on 14/05/15.
- */
-public class SimulatorWindowController {
-}
