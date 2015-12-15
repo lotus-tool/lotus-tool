afd3cdfb169bad5ae63f44a659f8e2fea2be13cc
lva <lucas.vieira@aluno.uece.br>
2015-12-15 15:56:00 -0300

    scronizacao

--- lotus-plugin-uml/src/main/java/br/uece/lotus/uml/api/ds/BlockDS.java
+++ lotus-plugin-uml/src/main/java/br/uece/lotus/uml/api/ds/BlockDS.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.uece.lotus.uml.api.ds;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author Bruno Barbosa
+ * Created by lva on 11/12/15.
  */
 public class BlockDS {
-    
+    private double mLayoutY;
+    private final List<Listener> mListeners = new ArrayList<>();
+    private double mLayoutX;
+
+
+    /*private final Component mComponent;*/
+
+    public interface Listener{
+        void onChange( BlockDS diagrama);
+    }
+
+    public double getLayoutX() {
+        return mLayoutX;
+    }
+    public double getLayoutY() {
+        return mLayoutY;
+    }
+
+    public void setLayoutX(double layoutX) {
+        mLayoutX = layoutX;
+        for (Listener l : mListeners) {
+            l.onChange(this);
+        }
+    }
+    public void setLayoutY(double layoutY) {
+        mLayoutY = layoutY;
+        for (Listener l : mListeners) {
+            l.onChange(this);
+        }
+    }
+    public void addListener(Listener l) {
+        mListeners.add(l);
+    }
+
+    public void removeListener(Listener l) {
+        mListeners.remove(l);
+    }
 }
+
