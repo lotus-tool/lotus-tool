004488910a8e007ce74ba3e22bba6953d3e0371a
LVA <lucas.vieira@aluno.uece.br>
2016-01-06 19:37:57 -0300

    modificacoes na pasta ds

--- lotus-plugin-uml/src/main/java/br/uece/lotus/uml/api/ds/BlockDS.java
+++ lotus-plugin-uml/src/main/java/br/uece/lotus/uml/api/ds/BlockDS.java
@@ -1,28 +1,43 @@
 package br.uece.lotus.uml.api.ds;
 
+import br.uece.lotus.Transition;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lva on 11/12/15.
  */
 public class BlockDS {
+    private final List<Transition> mTransicoesSaida = new ArrayList<>();
+    private final List<Transition> mTransicoesEntrada = new ArrayList<>();
+    private final Map<String, Object> mValues = new HashMap<>();
+    private double mLayoutX;
     private double mLayoutY;
     private final List<Listener> mListeners = new ArrayList<>();
-    private double mLayoutX;
+    private final ComponentDS componentDS;
+    private int mID;
+    private String mColor;
+    private String mBorderColor;
+    private Integer mBorderWidth;
+    private String mTextColor;
+    private Integer mTextSize;
+    private String mTextStyle;
+    private String mLabel;
+    private int mVisitedBlockDSCount;
 
+    BlockDS(ComponentDS c) {
+        componentDS =c;}
 
-    /*private final Component mComponent;*/
+    public ComponentDS getComponentDS(){return this.componentDS;}
 
-    public interface Listener{
-        void onChange( BlockDS diagrama);
-    }
+    public void setID(int id){mID =id;}
+
+    public int getID(){return mID;}
 
     public double getLayoutX() {
         return mLayoutX;
-    }
-    public double getLayoutY() {
-        return mLayoutY;
     }
 
     public void setLayoutX(double layoutX) {
@@ -31,18 +46,225 @@ public class BlockDS {
             l.onChange(this);
         }
     }
+
     public void setLayoutY(double layoutY) {
         mLayoutY = layoutY;
         for (Listener l : mListeners) {
             l.onChange(this);
         }
     }
+
+    public String getColor(){return mColor;}
+
+    public void setColor(String color){
+        mColor =color;
+        for(Listener l: mListeners){
+            l.onChange(this);
+        }
+    }
+
+    public String getBorderColor(){
+        return mBorderColor;
+    }
+
+    public void setmBorderColor(String color){
+        mBorderColor=color;
+        for (Listener l: mListeners){
+            l.onChange(this);
+        }
+    }
+
+    public Integer getBorderWidth() {
+        return mBorderWidth;
+    }
+
+    public void setBorderWidth(Integer width) {
+        mBorderWidth = width;
+        for (Listener l : mListeners) {
+            l.onChange(this);
+        }
+    }
+
+
+    public double getLayoutY() {
+        return mLayoutY;
+    }
+
+    public interface Listener{
+        void onChange( BlockDS diagrama);
+    }
+
+    public String getTextColor() {
+        return mTextColor;
+    }
+
+    public void setTextColor(String color) {
+        mTextColor = color;
+        for (Listener l : mListeners) {
+            l.onChange(this);
+        }
+    }
+
+    public Integer getTextSize() {
+        return mTextSize;
+    }
+
+    public void setTextSize(Integer size) {
+        mTextSize = size;
+        for (Listener l : mListeners) {
+            l.onChange(this);
+        }
+    }
+
+    public String getTextStyle() {
+        return mTextStyle;
+    }
+
+    public void setTextSyle(String color) {
+        mTextStyle = color;
+        for (Listener l : mListeners) {
+            l.onChange(this);
+        }
+    }
+    public String getLabel() {
+        return mLabel;
+    }
+
+    public void setLabel(String label) {
+        mLabel = label;
+        for (Listener l : mListeners) {
+            l.onChange(this);
+        }
+    }
+
+    public Transition getTransitionTo(BlockDS ds) {
+        for (Transition t : mTransicoesSaida) {
+            if (t.getDestiny().equals(ds)) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public List<Transition> getTransitionsTo(BlockDS ds) {
+        List<Transition> r = new ArrayList<>();
+        for (Transition t : mTransicoesSaida) {
+            if (t.getDestiny().equals(ds)) {
+                r.add(t);
+            }
+        }
+        return r;
+    }
+
+    public Transition getTransitionByLabel(String label) {
+        for (Transition t : mTransicoesSaida) {
+            if (t.getLabel().equals(label)) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    void copy(BlockDS ds) {
+        mID = ds.mID;
+        mBorderColor = ds.mBorderColor;
+        mBorderWidth = ds.mBorderWidth;
+        mColor = ds.mColor;
+        mLabel = ds.mLabel;
+        mLayoutX = ds.mLayoutX;
+        mLayoutY = ds.mLayoutY;
+        mTextColor = ds.mTextColor;
+        mTextSize = ds.mTextSize;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + this.mID;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BlockDS other = (BlockDS) obj;
+        if (this.mID != other.mID) {
+            return false;
+        }
+        return true;
+    }
+    public Object getValue(String key) {
+        return mValues.get(key);
+    }
+
+    public void setValue(String key, Object value) {
+        mValues.put(key, value);
+    }
+
+    public int getmVisitedBlockDSCount() { return mVisitedBlockDSCount; }
+
+    public void setmVisitedBlockDSCount(int value) { mVisitedBlockDSCount = value; }
+
+    public void incrementStatesCount() { mVisitedBlockDSCount++; }
+
+    public void decrementStatesCount() { mVisitedBlockDSCount--; }
+
+   /* public Iterable<Transition> getIncomingTransitions() {
+        return mTransicoesEntrada;
+    }
+
+    public Iterable<Transition> getOutgoingTransitions() {
+        return mTransicoesSaida;
+    }
+
+    public List<Transition> getIncomingTransitionsList() {
+        return Collections.unmodifiableList(mTransicoesEntrada);
+    }
+
+    public List<Transition> getOutgoingTransitionsList() {
+        return Collections.unmodifiableList(mTransicoesSaida);
+    }*/
+
+
+
+
+
+
     public void addListener(Listener l) {
         mListeners.add(l);
     }
 
     public void removeListener(Listener l) {
         mListeners.remove(l);
+    }
+
+    void addIncomingTransition(Transition t) {
+        mTransicoesEntrada.add(t);
+    }
+
+    void addOutgoingTransition(Transition t) {
+        mTransicoesSaida.add(t);
+    }
+
+    void removeIncomingTransition(Transition transition) {
+        mTransicoesEntrada.remove(transition);
+    }
+
+    void removeOutgoingTransition(Transition transition) {
+        mTransicoesSaida.remove(transition);
+    }
+
+    public int getOutgoingTransitionsCount() {
+        return mTransicoesSaida.size();
+    }
+
+    public int getIncomingTransitionsCount() {
+        return mTransicoesEntrada.size();
     }
 }
 
--- lotus-plugin-uml/src/main/java/br/uece/lotus/uml/api/ds/ComponentDS.java
+++ lotus-plugin-uml/src/main/java/br/uece/lotus/uml/api/ds/ComponentDS.java
@@ -6,57 +6,352 @@
 package br.uece.lotus.uml.api.ds;
 
 import br.uece.lotus.Component;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 /**
- *
  * @author Bruno Barbosa
  */
 public class ComponentDS {
 
-    
-    public interface Listener{
+    private List<Component> mComponentsLTS = new ArrayList<>();
+    private final Map<String, Object> mValues = new HashMap<>();
+    private boolean mAutoUpdateLabels = true;
+    private String mName;
+
+    public interface Listener {
         void onChange(ComponentDS cds);
+        void onBlockDSCreated(ComponentDS componentDS, BlockDS state);
+        void onBlockDSRemoved(ComponentDS componentDS, BlockDS state);
         //falta adicionar os outros listener. Quando for adicionar me avisa, pq tem que implementar o restante
         //que falta nas outras classes que dependen desses listener. se nao ja vai dar erro na compilacao ou na abertura
         //da tela
     }
-    
-    private List<Component> mComponentsLTS = new ArrayList<>();
-    private final List<Listener> mListeners = new ArrayList<>();
-    private String mName;
-    
-    
-    
-    public List<Component> getmComponentsLTS() {
-        return mComponentsLTS;
+
+
+
+    public Object getValue(String key) {
+        return mValues.get(key);
     }
 
+    public void setValue(String key, Object value) {
+        mValues.put(key, value);
+    }
+    private void copyBlockDS(BlockDS from, BlockDS to) {
+        to.setLabel(from.getLabel());
+        to.setLayoutX(from.getLayoutX());
+        to.setLayoutY(from.getLayoutY());
+    }
+
+    public List<Component> getmComponentsLTS() {return mComponentsLTS;}
     public void setmComponentsLTS(List<Component> mComponentsLTS) {
         this.mComponentsLTS = mComponentsLTS;
     }
-    
-    public void addComponentLTS(Component c){
+    public void addComponentLTS(Component c) {
         mComponentsLTS.add(c);
     }
-    public void removeComponentLTS(Component c){
+    public void removeComponentLTS(Component c) {
         mComponentsLTS.remove(c);
     }
-    
-    public void setName(String s) {
-        this.mName = s;
+
+///////////////////////////////////////////////////////////////////////////
+    //    public void setName(String s) {
+//        this.mName = s;
+//    }
+//
+//    public String getName() {
+//        return this.mName;
+//    }
+//
+//    public void addListener(Listener l) {
+//        mListeners.add(l);
+//    }
+//
+//    public void removeListener(Listener l) {
+//        mListeners.remove(l);
+//    }
+///////////////////////////////////////////////////////////////////////
+
+    private final List<BlockDS> mBlockDSs = new ArrayList<>();
+    /*  private final List<Transition> mTransitions = new ArrayList<>();*/
+    private final List<Listener> mListeners = new ArrayList<>();
+
+    public String getName() {return mName;}
+
+    public Iterable<BlockDS> getBlockDS() {return mBlockDSs;}
+
+    public int getBlockDSCount() {return mBlockDSs.size();}
+
+    public int getBlockDSIndex(BlockDS s) {return mBlockDSs.indexOf(s);}
+
+    public void setName(String name) {
+        mName = name;
+        for (Listener l : mListeners) {
+            l.onChange(this);
+        }
+    }
+    public int getBigStatesCount() {
+        int count = 0;
+        for (BlockDS s : mBlockDSs) {
+            /*if(s.getValue("bigstate")!=null)
+                count++;*/
+        }
+        return count;
     }
 
-    public String getName() {
-        return this.mName;
+
+
+   /* public Iterable<Transition> getTransitions() {
+        return mTransitions;
+    }*/
+
+   /* public int getTransitionsCount() {
+        return mTransitions.size();
+    }*/
+
+    public BlockDS getStateByID(int id) {
+        for (BlockDS v : mBlockDSs) {
+            if (v.getID() == id) {
+                return v;
+            }
+        }
+        return null;
     }
-    
+
+    public BlockDS newBlockDS(int id) {
+        BlockDS v = new BlockDS(this);
+        v.setID(id);
+        add(v);
+        /*if (mInitialState == null) {
+            setInitialState(v);
+        }*/
+        return v;
+    }
+
+    public void add(BlockDS v) {
+        mBlockDSs.add(v);
+        updateStateLabels();
+        for (Listener l : mListeners) {
+            l.onBlockDSCreated(this, v);
+        }
+    }
+
+   /* public void add(Transition t) {
+        t.getSource().addOutgoingTransition(t);
+        t.getDestiny().addIncomingTransition(t);
+        mTransitions.add(t);
+        for (Listener l : mListeners) {
+            l.onTransitionCreated(this, t);
+        }
+    }*/
+
+    /*public Transition newTransition(State src, State dst) {
+        if (src == null) {
+            throw new IllegalArgumentException("src state can't be null!");
+        }
+        if (dst == null) {
+            throw new IllegalArgumentException("dst state can't be null!");
+        }
+        Transition t = new Transition(src, dst);
+        add(t);
+        return t;
+    }
+
+    public Transition newTransition(int idSrc, int idDst) {
+        return newTransition(getStateByID(idSrc), getStateByID(idDst));
+    }
+
+    public Transition.Builder buildTransition(State src, State dst) {
+        if (src == null) {
+            throw new IllegalArgumentException("src state can't be null!");
+        }
+        if (dst == null) {
+            throw new IllegalArgumentException("dst state can't be null!");
+        }
+        Transition t = new Transition(src, dst);
+        return new Transition.Builder(this, t);
+    }
+
+    public Transition.Builder buildTransition(int idSrc, int idDst) {
+        Transition t = new Transition(getStateByID(idSrc), getStateByID(idDst));
+        return new Transition.Builder(this, t);
+    }*/
+
+    public void remove(BlockDS ds) {
+        /*List<Transition> transitions = new ArrayList<>();
+        transitions.addAll(ds.getOutgoingTransitionsList());
+        transitions.addAll(ds.getIncomingTransitionsList());
+        for (Transition t : transitions) {
+            remove(t);
+        }*/
+
+        mBlockDSs.remove(ds);
+        for (Listener l : mListeners) {
+            l.onBlockDSRemoved(this, ds);
+        }
+        /*if (ds.isInitial()) {
+            if (mStates.size() > 0) {
+                setInitialState(mStates.get(0));
+            }
+        }*/
+        updateStateLabels();
+    }
+
+    private void updateStateLabels() {
+        if (!mAutoUpdateLabels) {
+            return;
+        }
+        int i = 0;
+        for (BlockDS v : mBlockDSs) {
+            v.setLabel(String.valueOf(i++));
+        }
+    }
+
+   /* public Transition getTransitionByLabel(String label){
+        for(Transition transition : mTransitions){
+            if(label.equals(transition.getLabel())){
+                return transition;
+            }
+        }
+        return null;
+    }*/
+
+   /* public void remove(Transition transition) {
+        transition.getSource().removeOutgoingTransition(transition);
+        transition.getDestiny().removeIncomingTransition(transition);
+        mTransitions.remove(transition);
+        for (Listener l : mListeners) {
+            l.onTransitionRemoved(this, transition);
+        }
+    }*/
+
     public void addListener(Listener l) {
         mListeners.add(l);
     }
 
     public void removeListener(Listener l) {
         mListeners.remove(l);
+    }
+
+/*    public State getInitialState() {
+        return mInitialState;
+    }*/
+
+    /*public void setInitialState(State initialState) {
+        if (mInitialState != null) {
+            mInitialState.markInitial(false);
+        }
+        mInitialState = initialState;
+        if (mInitialState != null) {
+            mInitialState.markInitial(true);
+        }
+    }
+
+    public State getErrorState() {
+        return mErrorState;
+    }
+
+    public void setErrorState(State errorState) {
+        if (mErrorState != null) {
+            mErrorState.setError(false);
+        }
+        mErrorState = errorState;
+        if (mErrorState != null) {
+            mErrorState.setError(true);
+        }
+    }
+
+    public State getFinalState() {
+        return mFinalState;
+    }
+
+    public void setFinalState(State finalState) {
+        if (mFinalState != null) {
+            mFinalState.setFinal(false);
+        }
+        mFinalState = finalState;
+        if (mFinalState != null) {
+            mFinalState.setFinal(true);
+        }
+    }*/
+
+    public void clearVisitedBlockDSCount() {
+        for (BlockDS s : mBlockDSs) {
+            s.setmVisitedBlockDSCount(0);
+        }
+    }
+
+    @Override
+    public ComponentDS clone() throws CloneNotSupportedException {
+        ComponentDS c = new ComponentDS();
+        c.mName = mName;
+        c.mAutoUpdateLabels = mAutoUpdateLabels;
+        for (BlockDS oldState : mBlockDSs) {
+            BlockDS newState = c.newBlockDS(oldState.getID());
+            copyBlockDS(oldState, newState);
+        }
+       /* for (Transition oldTransition : mTransitions) {
+            int src = oldTransition.getSource().getID();
+            int dst = oldTransition.getDestiny().getID();
+            Transition newTransition = c.newTransition(src, dst);
+            copyTransition(oldTransition, newTransition);
+        }*/
+
+      /*  if (mInitialState != null) {
+            c.setInitialState(c.getStateByID(mInitialState.getID()));
+        }
+        if (mFinalState != null) {
+            c.setFinalState(c.getStateByID(mFinalState.getID()));
+        }
+        if (mErrorState != null) {
+            c.setErrorState(c.getStateByID(mErrorState.getID()));
+        }*/
+        return c;
+    }
+
+    public void setAutoUpdateLabels(boolean value) {
+        mAutoUpdateLabels = value;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.mName);
+       /* hash = 59 * hash + Objects.hashCode(this.mInitialState);
+        hash = 59 * hash + Objects.hashCode(this.mFinalState);
+        hash = 59 * hash + Objects.hashCode(this.mErrorState);*/
+        hash = 59 * hash + Objects.hashCode(this.mBlockDSs);
+      /*  hash = 59 * hash + Objects.hashCode(this.mTransitions);*/
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ComponentDS other = (ComponentDS) obj;
+        if (!Objects.equals(this.mName, other.mName)) {
+            return false;
+        }
+        /*if (!Objects.equals(this.mInitialState, other.mInitialState)) {
+            return false;
+        }
+        if (!Objects.equals(this.mFinalState, other.mFinalState)) {
+            return false;
+        }
+        if (!Objects.equals(this.mErrorState, other.mErrorState)) {
+            return false;
+        }
+        if (!Objects.equals(this.mStates, other.mStates)) {
+            return false;
+        }
+        if (!Objects.equals(this.mTransitions, other.mTransitions)) {
+            return false;
+        }*/
+        return true;
     }
 }
