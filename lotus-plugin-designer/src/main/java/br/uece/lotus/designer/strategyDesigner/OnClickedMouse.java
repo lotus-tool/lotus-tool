d3e2d5a7866e538270e36bccc05858040cc9798e
Yan Gurgel <Yan Gurgel@10.129.4.22>
2017-05-02 11:25:34 -0300

    Delecção aprimorada e Simulador melhorado

--- lotus-plugin-designer/src/main/java/br/uece/lotus/designer/strategyDesigner/OnClickedMouse.java
+++ lotus-plugin-designer/src/main/java/br/uece/lotus/designer/strategyDesigner/OnClickedMouse.java
@@ -17,9 +17,10 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javax.swing.JOptionPane;
-
+ 
 /**
  * Created by lva on 19/11/15.
+ * Adaptation by Yan Gurgel 12/2016
  */
 public class OnClickedMouse implements Strategy {
 
@@ -107,21 +108,75 @@ public class OnClickedMouse implements Strategy {
                     }
                 }
             } else if (dwi.mModoAtual == MODO_REMOVER) {
-                /*if there are states that are selected and the eraser button was chosen 
-                        so erase all selected transitions from the component*/
+                /*
+                *Verifica se existe algum estado selecionado após clicar na borracha se sim ele(s)
+                *são apagados
+                */
                 if (!dwi.statesSelecionados.isEmpty()) {
                     int r = JOptionPane.showConfirmDialog(null,
-                            new StringBuilder("Do you really want to remove the all selection?")/*.append(dwi.mComponentSobMouse instanceof StateView ? "State" : dwi.mComponentSobMouse instanceof TransitionView ? "Transition" : "").append("?")*/,
+                            new StringBuilder("Do you really want to remove the all selection?"),
                             "Remove",
                             JOptionPane.YES_NO_OPTION);
-
                     if (r == JOptionPane.YES_OPTION) {
                         for (State s : dwi.statesSelecionados) {
                             dwi.mViewer.getComponent().remove(s);
                         }
                         dwi.statesSelecionados.clear();
                     }
-                } else if (dwi.mComponentSelecionado != null && dwi.mComponentSobMouse != dwi.getSelectedView() && (dwi.mComponentSobMouse instanceof StateView || dwi.mComponentSobMouse instanceof TransitionView)) {
+                    else{
+                        for (State s : dwi.statesSelecionados) {
+                            //dwi.removeSelectedStyles(s);
+                             s.setBorderWidth(1);
+                             s.setBorderColor("black");
+                             s.setTextColor("black");
+                             s.setTextSyle(State.TEXTSTYLE_NORMAL);
+                        }
+                        dwi.statesSelecionados.clear();
+                    }
+                } 
+                /*
+                *Verifica se existe alguma Transition selecionada após clicar na borracha 
+                *se sim ela é apagada
+                */ 
+                else if (dwi.mComponentSelecionado != null && dwi.getSelectedView() instanceof TransitionView) {
+                    int r = JOptionPane.showConfirmDialog(null,
+                            new StringBuilder("Do you really want to remove the Transition?"), "Remove",
+                            JOptionPane.YES_NO_OPTION);
+
+                    if (r == JOptionPane.YES_OPTION) {
+
+                        Transition t = ((TransitionView) dwi.getSelectedView()).getTransition();
+                        State iniTransition = t.getSource();
+                        State fimTransition = t.getDestiny();
+                        dwi.mViewer.getComponent().remove(t);
+                        //Verificar Mais de uma Trasition do mesmo Source e Destiny
+                        List<Transition> multiplasTransicoes = iniTransition.getTransitionsTo(fimTransition);
+                        if (multiplasTransicoes.size() > 0) {
+                            //deletar da tela
+                            for (Transition trans : multiplasTransicoes) {
+                                dwi.mViewer.getComponent().remove(trans);
+                            }
+                            //recriar transitions
+                            for (Transition trans : multiplasTransicoes) {
+                                dwi.mViewer.getComponent().buildTransition(iniTransition, fimTransition)
+                                        .setGuard(trans.getGuard())
+                                        .setLabel(trans.getLabel())
+                                        .setProbability(trans.getProbability())
+                                        .setViewType(TransitionView.Geometry.CURVE)
+                                        .create();
+                            }
+                        }
+                        dwi.mComponentSelecionado = null;
+                    }
+                    else{
+                    dwi.setComponenteSelecionado(null);
+                    dwi.mComponentSelecionado = null;
+                    }
+                }
+                /*
+                *Deleção caso não existe uma seleção (caso 1)
+                */ 
+                else if (dwi.mComponentSelecionado != null && dwi.mComponentSobMouse != dwi.getSelectedView() && (dwi.mComponentSobMouse instanceof StateView || dwi.mComponentSobMouse instanceof TransitionView)) {
                     int r = JOptionPane.showConfirmDialog(null,
                             new StringBuilder("Do you really want to remove the ").append(dwi.mComponentSobMouse instanceof StateView ? "State" : dwi.mComponentSobMouse instanceof TransitionView ? "Transition" : "").append("?"),
                             "Remove",
@@ -175,8 +230,12 @@ public class OnClickedMouse implements Strategy {
                         }
 
                     }
-                } //====================================================================================================================================
-                else if (dwi.mComponentSobMouse != null && dwi.mComponentSelecionado == null && (dwi.mComponentSobMouse instanceof StateView || dwi.mComponentSobMouse instanceof TransitionView)) {
+                } 
+//====================================================================================================================================
+                /*
+                *Deleção caso não existe uma seleção (caso 2)
+                */ 
+                 else if (dwi.mComponentSobMouse != null && dwi.mComponentSelecionado == null && (dwi.mComponentSobMouse instanceof StateView || dwi.mComponentSobMouse instanceof TransitionView)) {
                     dwi.getComponent().getName();
                     int r = JOptionPane.showConfirmDialog(null,
                             new StringBuilder("Do you really want to remove the ").append(dwi.mComponentSobMouse instanceof StateView ? "State" : dwi.mComponentSobMouse instanceof TransitionView ? "Transition" : "").append("?"),
@@ -233,7 +292,6 @@ public class OnClickedMouse implements Strategy {
             }
         }
     }
-
     @Override
     public void onMovedMouse(DesignerWindowImpl Dwi, MouseEvent event) {
     }
